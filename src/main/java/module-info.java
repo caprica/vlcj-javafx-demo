c972ed7fabf2b7c74357b33594278f12ef214913
Mark Lee <mark.lee@capricasoftware.co.uk>
2020-04-29 10:35:06 +0100

    Purge Java Modules system as much as possible

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module uk.co.caprica.vlcj.javafx.demo {
-    requires javafx.graphics;
-    requires uk.co.caprica.vlcj;
-    requires uk.co.caprica.vlcj.javafx;
-    requires javafx.controls;
-
-    opens uk.co.caprica.vlcj.javafx.demo to javafx.graphics;
-}
